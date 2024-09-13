6769bdae420974ca0ac91a510d4410a06f217688
Edmore Moyo <etmoyo@gmail.com>
2024-09-13 15:14:49 -0400

    removes old code generation code

--- main.py
+++ main.py
@@ -7,78 +7,10 @@ import pdfkit
 import datetime
 import pytz
 
-def extractSubject(file):
-    val = file.find('.')
-    if val == -1:
-        return file
-    return file.split('.')[0]
-
-def rotateXaxis(v, rotation):
-    for axes in v.axes.flat:
-        _ = axes.tick_params(axis='x', labelrotation = rotation)
-
-    return v
-
-def relplot(data, x_axis, y_axis, hue, kind, hue_order):
-    return sns.relplot(data=data, x=x_axis, y=y_axis, hue=hue, kind=kind, hue_order=hue_order)
-
 print("start of processing")
 
 src = os.environ['INPUT_DIR']
 dest = os.environ['OUTPUT_DIR']
-
-source_files = os.listdir(src)
-
-print("source files: ", source_files)
-
-if 'feat_major' in source_files and 'feat_adaptive' in source_files:
-    print('feat_major and feat_adaptive present in the list')
-    feat_major_files = os.listdir(f'{src}/feat_major')
-    feat_adaptive_files = os.listdir(f'{src}/feat_adaptive')
-
-    for filename in feat_major_files:
-        feat_major_df = pd.read_csv(f'{src}/feat_major/{filename}')
-        feat_adaptive_df = pd.read_csv(f'{src}/feat_adaptive/{filename}')
-
-        merged = feat_major_df.merge(feat_adaptive_df, on='file')
-        merged.to_csv(f'{src}/{filename}', index=False)
-
-dataframes = []
-for filename in source_files:
-    if "csv" in filename:
-        df = pd.read_csv(f'{src}/{filename}')
-        df['subject'] = extractSubject(df["file"].squeeze())
-        dataframes.append(df)
-data = pd.concat(dataframes, axis=0)
-
-sorted_data = data.sort_values("file")
-sorted_data.to_csv(f'{dest}/merged_results.csv')
-
-sns.set_theme(style="white")
-
-hue_order = sorted_data['file'].to_list()
-subject_hue_order = sorted_data['subject'].to_list()
-
-df_melt = pd.melt(sorted_data, id_vars=['file', 'subject'], var_name='cell_type', value_name='cell_distribution')
-print(df_melt)
-tcell_sorted_data = sorted_data[["file","T cell","T cell CD4", "subject"]]
-df_melt_tcell = pd.melt(tcell_sorted_data, id_vars=['file', 'subject'], var_name='cell_type', value_name='cell_distribution')
-print(df_melt_tcell)
-
-# plots
-raw_scatter = relplot(df_melt, "cell_type", "cell_distribution", "file", "scatter", hue_order)
-rotateXaxis(raw_scatter, 90).savefig(f'{dest}/raw_scatter.png')
-
-scatter_location = f'{dest}/scatter.png'
-scatter = relplot(df_melt, "cell_type", "cell_distribution", "subject", "scatter", subject_hue_order)
-rotateXaxis(scatter, 90).savefig(scatter_location)
-
-t_cell_scatter_location = f'{dest}/tcell_scatter.png'
-tcell_scatter = relplot(data, "T cell", "T cell CD4", "subject", "scatter", subject_hue_order)
-rotateXaxis(tcell_scatter, 90).savefig(t_cell_scatter_location)
-
-raw_tcell_scatter = relplot(data, "T cell", "T cell CD4", "file", "scatter", hue_order)
-rotateXaxis(raw_tcell_scatter, 90).savefig(f'{dest}/raw_tcell_scatter.png')
 
 # create report - pdfkit
 banner = "/service/I3H_Logo_HiRes.jpg"
@@ -114,7 +46,7 @@ body = """
         <td>Cell classification across across all included subjects. Only selected classifications are included in this graph.</td>
         </tr>
         <tr style='padding-bottom:50px'>
-        <td style='text-align:center; vertical-align:middle'><img width="450px" src="{scatter_location}"></td>
+        <td style='text-align:center; vertical-align:middle'><img width="450px"></td>
         </tr>
         <tr>
             <td><b>2. T cell CD4 plotted against total T cells<b></td>
@@ -123,14 +55,14 @@ body = """
         <td>Graphing CD4 T-cell populations against total T-Cell populations across all included subjects.</td>
         </tr>
         <tr style='padding-bottom:50px'>
-            <td style='text-align:center; vertical-align:middle'><img width="450px" src="{t_cell_scatter_location}"></td>
+            <td style='text-align:center; vertical-align:middle'><img width="450px"></td>
         </tr>
       </table>
       </body>
       <br>
       <footer><center>421 Curie Blvd. Philadelphia, PA 19104</center></footer>
       </html>
-    """.format(banner=banner, generated=generated, scatter_location=scatter_location, t_cell_scatter_location=t_cell_scatter_location)
+    """.format(banner=banner, generated=generated)
 
 options = {"enable-local-file-access": ""}
 pdfkit.from_string(body, f'{dest}/ih-report.pdf', options=options)
